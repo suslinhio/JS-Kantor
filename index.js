570fb921859cf8793f1da5927c822621a2cd99ab
suslinhio <suslinhio@gmail.com>
2023-10-03 19:44:05 +0300

    Основи JavaScript.Перетворення типу.

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 'use strict' 
 
-alert('hello');
+// let value = true;
+// alert(typeof value);
 
-let userName = prompt('What is your name?', 'Lox');
-alert(`Hello, ${userName}!`);
+// value = String(value);
+// alert(typeof value);
 
-let someBear = confirm('Wonna some beer?');
+/* alert('6' / '3');
+
+let string = '123';
+alert(typeof string);
+
+string = Number(string);
+alert(typeof string);
+
+Якщо рядок не є дійсним числом, результатом такого перетворення є NaN. */
+
+// let string = 'I dont know what to type';
+// string = Number(string);
+// alert(typeof string);
+// alert(string); // NaN
+
+// undefined -	NaN
+// null	- 0
+// true та false	- 1 та 0
+// string	Пробільні символи (пробіли, символи табуляції \t, символи нового рядку \n тощо) на початку та з кінця видаляються. Якщо рядок, що залишився в результаті, порожній, то результатом є 0. В іншому випадку число “читається” з рядка. Помилка дає NaN.
+
+// alert( Boolean(1) ); // true
+// alert( Boolean(0) ); // false
+
+// alert( Boolean("вітаю") ); // true
+// alert( Boolean("") ); // false
