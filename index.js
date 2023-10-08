27778ec2ca9ae36c3cdb86cae8a236ec3cd5bab9
suslinhio <suslinhio@gmail.com>
2023-10-08 18:48:35 +0300

    Основи JavaScript.Умовні розгалуження: if, '?'

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
 'use strict' 
 
-// Оператори порівняння повертають значення логічного типу.
-// Рядки порівнюються посимвольно в лексикографічному порядку.
-// Значення різних типів під час порівняння конвертуються в числа. Винятками є порівняння за допомогою операторів строгої рівності/нерівності.
-// Значення null і undefined рівні == один одному і не рівні будь-якому іншому значенню.
-// Будьте обережні, використовуючи оператори порівняння на зразок > чи < зі змінними, які можуть приймати значення null/undefined. Хорошою ідеєю буде зробити окрему перевірку на null/undefined для таких значень.
+// Умовний оператор ‘?’ - тернарний оператор, єдиний маж три операнди
+// let resalt = condition ? output1 : output2;
 
-5 > 4 // true
-"ананас" > "яблуко" //false
-"2" > "12" //true
-undefined == null //true
-undefined === null //false
-null == "\n0\n" //false (null equals only undefined)
-null === +"\n0\n" //false
+// Without '?'
+/* 
+let accessAllowed = null;
+let age = Number(prompt('How old are you&'));
+
+if (age >= 18) {
+    accessAllowed = true;
+} else {
+    accessAllowed = false;
+}
+
+alert(accessAllowed); */
+
+// With '?'
+/* 
+let age = Number(prompt('How old are you&'));
+let accessAllowed = (age >= 18) ? alert('Want some beer?') : alert('Go do your homework!'); */
+
+
+// Декілька ‘?’
+/* 
+let age = Number(prompt('How old are you?'));
+
+let message = (age < 3) ? 'You are a todler!' :
+    (age < 18) ? 'You are a teenager!' :
+    (age < 50) ? 'You are adult!' :
+    (age < 99) ? 'You are very old.' :
+    'How the fuck are you still alive?';
+
+alert(message); */
+
+// Task 1
+// if ("0") {
+//     alert( 'Привіт' ); //yes
+//   }
+
+// Task 2 Назва JavaScript
+
+/* let jsName = prompt('What is official name of JavaScript?');
+
+if (jsName === 'ECMAScript') {
+    alert('You are so clever!');
+} else {
+    alert('You have never heard ofECMAScript???');
+} */
+
+// Task 3
+
+/* let num = Number(prompt('Insert namber'));
+
+if (num > 0) {
+    alert('1');
+} else if (num < 0) {
+    alert('-1');
+} else {
+    alert('0');
+} */
+
+// Task 4
+
+/* let result = (a + b < 4) ? 'Smaller' : 'Bigger';
+alert(result); */
+
+// Task 5
+
+let message = (login === 'worker') ? 'Hello' :
+    (login === 'director') ? 'Good afternoon' :
+    (login === ' ') ? 'Entry denied' :
+    '';
+
