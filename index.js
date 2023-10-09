18095a3df299f683fc545f5c1b136c0b8066ef0a
suslinhio <suslinhio@gmail.com>
2023-10-09 18:51:35 +0300

    Основи JavaScript. Цикли: while і for.

--- index.js
+++ index.js
@@ -1,29 +1,124 @@
 'use strict' 
 
-// Оператор об’єднання з null '??'
+// Цикли: while і for
 
-/* 
-Результатом a ?? b буде:
+// Цикл – це спосіб повторити один і той же код кілька разів.
 
-a, якщо a визначене,
-b, якщо a не визначене.
+/* WHILE
 
-Інакше кажучи, ?? повертає перший аргумент, якщо він не null/undefined. Інакше, другий.
+while (condition) {
+    loop body
+}
 
-Зручно для показу нікнеймів ш тд, якщо зміннавизначена, то показуємо це значення, якщо ні, то якесь стандартне.
 */
 
-let userName = null;
-let userLastname = null;
-let userNickname = 'Supercoder'
-alert(userName ?? userLastname ?? userNickname ?? 'Guest'); // Supercoder
+/* let i = 0;
 
-// || may be used here as well
-alert(userName || userLastname || userNickname || 'Guest'); // Supercoder
+while (i < 3) {
+    console.log(i);
+    i++;
+}
+ */
+
+/* let i = 3;
+
+while (i) {
+    console.log(i);
+    i--;
+} */
+
+/* Цикл “do…while” 
+
+do {
+    loop body;
+} while (condition);
+
+Цю форму синтаксису слід використовувати лише тоді, коли ви хочете, щоб тіло циклу виконалось хоча б один раз, незалежно від умови.
+*/
 
 /* 
-Важлива різниця між ними полягає в тому, що:
+Цикл “for”
 
-|| повертає перше істинне значення.
-?? повертає перше визначене значення.
+for (initializing counter; condition; step) {
+    loop body;
+}
 */
+
+/* for (let i = 0; i < 3; i++) {
+    console.log(i);
+} */
+
+/* 
+Continue
+
+Зупиняє поточну ітерацію і починає виконання циклу спочатку з наступної ітерації (якщо умова циклу досі вірна).
+
+*/
+
+/* for (let i = 0; i < 10; i++) {
+    if (i % 2) {
+        console.log(i)
+    }
+} */
+
+/* for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) continue;
+    console.log(i); 
+} */
+
+// Мітки для break/continue
+
+/* 
+labelName: for (...) {
+  ...
+} 
+*/
+
+/* outer: 
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        let input = prompt(`The coordinates are: ${i}, ${j}`);
+        if (!input) break outer;
+    }
+}
+alert('Ready!'); */
+
+//TASKS
+
+/* let i = 0;
+while (++i < 5) alert( i ); // 1, 2, 3, 4
+
+let i = 0;
+while (i++ < 5) alert( i ); */ // 1, 2, ,3, 4, 5
+
+/* for (let i = 0; i < 5; i++) alert( i ); // 0, 1, 2, 3, 4
+for (let i = 0; i < 5; ++i) alert( i ); // 0, 1, 2, 3, 4
+ */
+
+/* for (let i = 2; i < 11; i++) {
+    if (i % 2) continue;
+    console.log(i);
+} */
+
+/* let i = 0;
+while (i < 3) {
+    console.log(`Number ${i}!`);
+    i++;
+} */
+
+/* for (;;) {
+    let userNum = Number(prompt('Insert number over 100.'));
+    if (userNum > 100) break;
+} */
+
+/* let num = 100;
+outer:
+for (let i = 2; i <= num; i++) {
+    
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) continue outer;
+        
+    }
+
+    console.log(i);
+} */
