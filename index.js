c04e2eb61d6de1b42c6f7dd2d88ee08377826e68
suslinhio <suslinhio@gmail.com>
2023-10-11 13:55:31 +0300

    Основи JavaScript. Фенкціональні вирази.

--- index.js
+++ index.js
@@ -2,85 +2,42 @@
 
 // Functions
 
-/* 
-Function declaration
+/* Function expression */
 
-function newFunction (args) {
+/* 
+Syntax:
+
+let variable = function(args) {
     function body;
 }
 
-Змінна, яка оголошена в функції доступна лише в тілі цієї функції. - local variable
-But, fункція може використовувати зовнішні змінні
+Функціональний Вираз буде створено тільки тоді, коли до нього дійде виконання і тільки після цього він може бути використаний.
 
-Зовнішня змінна використовується тоді, коли немає локальної. Якщо всередині функції є змінна з таким самим ім’ям, то вона перекриває зовнішню. 
 
-Ми можемо задати так зване “типове” значення параметра, яке використовуватиметься, якщо не задати аргумент. Для цього потрібно написати значення через =
-
-Функція з порожнім return, або без return повертає undefined
-
-Функції, які починаються з …
-
-"show" зазвичай щось показують.
-"get…" – повертають значення,
-"calc…" – щось обчислюють,
-"create…" – щось створюють,
-"check…" – щось перевіряють і повертають булеве значення.
+Ще однією особливістю Оголошення Функції на відміну від Функціонального виразу є її блокова область видимості. У суворому режимі, якщо Оголошення Функції знаходиться в блоці {...}, то функція доступна усюди всередині блоку. Але не зовні.
 
 */
 
-/* function showMessage () {
-    let message = 'Hi, Yolop!'; //local variable
-    alert(message);
-}
+/* 
+Колбеки (функції зворотного виклику) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-showMessage(); //works
-alert(message); // doest work */
+*/
 
-function showPrimes (n) {
-    for (let i = 2; i < n; i++) {
-        if (!isPrime(i)) continue;
-        console.log(i);
-    }
-} 
-
-function isPrime (n) {
-    for (let j = 2; j < n; j++) {
-        if (n % j === 0) {
-            return false;
-        } else {
-            return true;
-        }
+function ask(question, yes, no) { // typical func to show modal window
+    if (confirm(question)) {
+        yes();
+    } else {
+        no();
     }
 }
 
-// TASKS
-
-// 1
-function checkAge(age) {
-    return (age > 18) ? true : confirm('Do you have permission?');
-    // return (age > 18) || confirm('Do you have permission?');
-  }
-
-// 2
-function getMin (a, b) {
-    if (a > b) {
-        return a;
-    }
-    return b;
+function yes () {
+    alert('You have agreed!');
 }
 
-// 3 Напишіть функцію pow(x, n), яка повертає число x, піднесене до степеня n. Інакше кажучи, множить число x саме на себе n разів і повертає результат.
-
-/* function pow(x, n) {
-    return x**n;
-} */
-
-function pow(x, n) {
-    let result = x;
-
-    for (let i = 1; i < n; i++) {
-        result *= x;
-    }
-
-    return result;
+function no () {
+    alert('You have declined...');
 }
+
+// ask('Wonna enter?', yes, no);
+// Аргументи showOk та showCancel функції ask називаються функціями зворотного виклику або просто колбеками.
