35c1fcc1e49f46c0cce13b68bbbc37d8f77e4478
suslinhio <suslinhio@gmail.com>
2023-10-11 21:33:02 +0300

    Об’єкти: основи. Об’єкти.

--- index.js
+++ index.js
@@ -1,86 +1,194 @@
 'use strict' 
 
-// Automation testing
+// Objects
 
 /* 
-Керована поведінкою розробка (BDD) - і тести, і документація, і приклади використання.
-В BDD спочатку пишуть специфікацію, потім реалізацію. В результаті ми маємо і специфікацію, і код реалізації.
+Syntax:
 
-Специфікацію можна використовувати трьома способами:
+let user = new Object(); // синтаксис "конструктора об’єктів"
 
-Як Тести – вони гарантують, що код працює правильно.
-Як Документацію – назви describe та it описують, що робить функція.
-Як Приклади – тести – це фактично робочі приклади, що показують, як можна використовувати функцію.
+let user = {};  // синтаксис "літералу об’єкта"
 
+Остання властивість у списку може закінчуватися комою, Це називається “кінцевою” або “висячою” комою. Полегшує додавання/видалення/переміщення по властивостях, оскільки всі рядки стають однаковими.
+
+*/
+
+/* let user = {
+    name: 'Dima',
+    age: 23,
+    'likes beer': true,
+}
+
+console.log(user.name);
+
+user.isBoss = true; // add key and value
+console.log(user.isBoss);
+
+delete user.age; // delete key and value
+console.log(user);
+
+console.log(user["likes beer"]); // refer to keys of two words */
+
+//Квадратні дужки також дозволяють звернутися до властивості, ім’я якої може бути результатом виразу. Наприклад, ім’я властивості може зберігатися в змінній:
+
+/* let key = 'likes crisps';
+user.key = true;
+console.log(user.key); */
+
+/* let key = prompt('What info do you want?', 'name');
+
+alert(user[key]); */
+
+// Ми можемо використовувати квадратні дужки в літеральній нотації для створення ОБЧИСЛЮВАЛЬНОЇ ВЛАСТИВОСТІ.
+
+/* let fruit = prompt('Which fruit to add in my sholling list?', 'BEER!!!');
+
+let bag = {
+    [fruit]: 5,
+}
+
+console.log(bag[fruit]); */
+
+//У реальному коді ми часто використовуємо наявні змінні як значення для імен властивостей.
+
+/* function makeUser(name, age) {
+    return {
+        name: name, // may be shortened
+        age: age,
+    };
+} */
+
+/* function makeUser(name, age) {
+    return {
+        name,
+        age,
+    };
+}
+
+let newUser = makeUser('Ihor', 22);
+console.log(newUser); */
+
+//Є невеликий підводний камінь, пов’язаний зі спеціальною властивістю __proto__
+//Значення не присвоюється, повертає object Object
+// obj.__proto__ = 5; doesnt work
+
+// Перевірка існування властивості, оператор “in"
+
+/* console.log('name' in newUser); //true
+console.log('shkuuu' in newUser); //false */
+
+//ПЕРЕБОР ВСІХ ВЛАСТИВОСТЕЙ ОБЄКТА!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+/* 
+for .. in loop
 
 Syntax:
 
-describe('funcName', function() {
-
-    it('what is it doing', function() {
-        assert.equal(funcName(x, n), expectedResult);
-    });
-});
+for (key in object) {
+    loop body for EACH key;
+}
 */
 
-/* describe('pow', function() { // it-blocks written by hands
+/* let user = {
+    name: "Іван",
+    age: 30,
+    isAdmin: true
+  };
 
-    it('pows 2 to 3', function() {
-        assert.equal(pow(2, 3), 8);
-    });
+for (let key in user) { //(let prop in obj) is frequently used
+    console.log(key + ': '+ user[key]); 
+} */
 
-    it('pows 3 to 4', function() {
-        assert.equal(pow(3, 4), 81);
-    });
-}); */
-describe('pow', function() {
-    describe('pows x to 3', function() { 
+//Впорядкування властивостей об’єкта !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+/* 
+“властивості впорядковані особливим чином”. Властивості з цілочисельними ключами сортуються за зростанням, інші розташовуються в порядку створення.
 
-        function makeTest(x) { // automatic creation of tests
-            let expected = x * x * x;
-            it(`${x} powed to 3 equals ${expected}`, function() {
-                assert.equal(pow(x, 3), expected);
-            });
+Термін “цілочисельна властивість” означає рядок, який може бути перетворений в ціле число і назад без змін.
+
+Тобто, рядок "49" – це цілочисельне ім’я властивості, якщо його перетворити на ціле число, а потім назад в рядок, то воно не зміниться. А ось властивості "+49" або "1.2" такими не є.
+*/
+
+/* let codes = {
+    '49': 'Germany',
+    '41': 'Switzerland',
+    '44': 'Great Britain',
+    '1': 'USA',
+} */
+// Щоб це працювало, просто зробимо значення ключів нецілочисленними додавши + до кожного, тоді порядок перебору зміниться
+
+/* let codes = {
+    '+49': 'Germany',
+    '+41': 'Switzerland',
+    '+44': 'Great Britain',
+    '+1': 'USA',
+}
+
+for (let prop in codes) {
+    console.log(Number(prop));
+} */
+
+//TASKS
+
+/* Напишіть код, виконавши завдання з кожного пункту окремим рядком:
+
+Створіть порожній об’єкт user.
+Додайте властивість name зі значенням Іван.
+Додайте властивість surname зі значенням Сміт.
+Змініть значення name на Петро.
+Видаліть властивість name з об’єкта. */
+
+/* let user = {};
+
+user.name = 'Ivan';
+user.surname = 'Prylypko';
+user.name = 'Petro';
+delete user.name; */
+
+/* Напишіть функцію isEmpty(obj), яка повертає true, якщо об’єкт не має властивості, інакше false. */
+
+/* let schedule = {};
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+} */
+
+/* У нас є об’єкт для зберігання заробітної плати нашої команди:
+Напишіть код для підсумовування всіх зарплат і збережіть у змінній sum. У наведеному вище прикладі має бути 390.
+
+Якщо об’єкт salaries порожній, то результат має бути 0.
+*/
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+};
+
+function getSumOfSalaries (obj) {
+    let sum = 0;
+    for (let key in obj) {
+        sum += obj[key];
+    }
+    return sum;
+};
+
+let nosalaries = {};
+
+/* Створіть функцію multiplyNumeric(obj), яка примножує всі числові властивості об’єкта obj на 2. */
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (Number(obj[key]) === obj[key]) {
+            obj[key] *= 2;
         }
+    }
+};
 
-        for (let i = 0; i < 20; i++) {
-            makeTest(i);
-        }
-    });
-
-    describe('pows x to 2', function() { 
-
-        function makeTest(x) { 
-            let expected = x * x;
-            it(`${x} powed to 3 equals ${expected}`, function() {
-                assert.equal(pow(x, 2), expected);
-            });
-        }
-
-        for (let i = 0; i < 20; i++) {
-            makeTest(i);
-        }
-    });
-
-    describe('pows x to 4', function() { 
-
-        function makeTest(x) { 
-            let expected = x * x * x * x;
-            it(`${x} powed to 3 equals ${expected}`, function() {
-                assert.equal(pow(x, 4), expected);
-            });
-        }
-
-        for (let i = 0; i < 20; i++) {
-            makeTest(i);
-        }
-    });
-
-    it("для недійсних n результатом є NaN", function() {
-        assert.isNaN(pow(2, -1));
-      });
-    
-      it("для не цілих n результатом є NaN", function() {
-        assert.isNaN(pow(2, 1.5));
-      });
-});    
+let menu = {
+    width: 200,
+    height: 300,
+    title: "Моє меню"
+  };
