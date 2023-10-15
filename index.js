5cbc8708b84a5be9522f2e94c886d7127bff65cf
suslinhio <suslinhio@gmail.com>
2023-10-15 20:34:05 +0300

    this

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 'use strict' 
 
-// Збирання сміття
+// Методи об’єкту, "this"
 
 /* 
-Основна концепція управління пам’яттю в JavaScript – це досяжність.
+Функція, яка є властивістю об’єкта, називається його методом.
 
-Простіше кажучи, “досяжні” значення – це ті, які якимось чином доступні або придатні для використання.
-Обєкт вважається досяжним, якщо на нього є посилання з кореня(глобальні змінні, функції, що виконуються і т.д.), дуже схоже на хард лінки.
+Як правило, метод об’єкта повинен отримувати доступ до інформації, що зберігається в об’єкті, для виконання своєї роботи.
 */
+let user = {
+    name: 'Dima',
+    age: 23,
+}
+
+user.sayHi = function () {
+    console.log(`${user.name} says 'Hi!' to you!`);
+}
+
+// Стрілочні функції не мають “this”
+
+// Tasks
+
+//Creat obj calculator
+
+const calculator = {
+    read() {
+        this.a = Number(prompt('Insert first number.'));
+        this.b = Number(prompt('Insert second number.'));
+    },
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    },
+}
+
+//Chaining
+
+const ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep() {
+        console.log(this.step);
+        return this;
+    },
+}
