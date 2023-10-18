ecbde3ecf309104dd84e65fdf42e943227bbedf5
suslinhio <suslinhio@gmail.com>
2023-10-18 13:49:44 +0300

    Типи даних. Arrays

--- index.js
+++ index.js
@@ -1,145 +1,132 @@
 'use strict' 
 
-// Методи Strings
+// Arrays
 
-// .length - довжина рядка у символах (спеціяльні символи (\n) також рахуються)
+let fruits = ['Apple', 'Orange', 'Plum'];
 
-let str1 = 'Hello world!';
-console.log(str1.length);
-
-// Викликати символ за номером позиції у рядку можна кількома способами
-
-// 1 string[n]
-
-for (let i = 0; i < str1.length; i++) { // similar to arrays
-    console.log(str1[i]);
-}
-
-// 2 method string.at(pas)
-for (let i = -1; i > -str1.length; i--) { // similar to arrays
-    console.log(str1.at(i)); // Method at() can use negative nums => starts from the end
-}
-
-// 3 for ... of string
-for (let char of str1) {
-    console.log(char.toUpperCase());
-}
-
-// Methods toUpperCase() and toLowerCase()
-let str2 = 'BIG and small';
-console.log(str2.toUpperCase());
-console.log(str2.toLowerCase());
-
-
-// Методи для пошуку рядука
-
-// string.indexOf(substring, pos)
-// Він шукає підрядок substr в рядку str, починаючи з позиції pos, і повертає позицію, де знаходиться збіг, або -1 якщо збігів не було знайдено.
-
-console.log(str2.indexOf('sm', 2));
-
-/* Щоб знайти усі збіги, нам потрібно запустити indexOf в циклі. Кожен новий виклик здійснюється з позицією після попереднього збігу: */
-
-let stringToInvestigate = 'Хитрий, як лисиця, сильний, як Як';
-let target = 'як';
-
-let pos = -1;
-while ((pos = stringToInvestigate.indexOf(target, pos + 1)) != -1) {
-  alert( pos );
-}
-
-// string.lastIndexOf(substring, pos) - same, but starting reverse
-
-// includes(subctring, pos) - returns true/false whether substring exists
-
-console.log(stringToInvestigate.includes('як')); //true
-
-// str.startsWith(substring) та str.endsWith(substring) перевіряють, чи починається і чи закінчується рядок певним підрядком.
-
-console.log(stringToInvestigate.startsWith('Хит')); // true
-console.log(stringToInvestigate.startsWith('хит')); // false (watch out register)
-console.log(stringToInvestigate.endsWith('Як')); // true
-console.log(stringToInvestigate.endsWith('як')); // false (watch out register)
-
+// Щоб звернутися до останнього елементу є два варіанти
+// 1
+console.log(fruits[fruits.length - 1]);
+// 2
+console.log(fruits.at(-1));
 
 /* 
-Отримання підрядка
-В JavaScript є 3 метода для отримання підрядка: substring, substr та slice.
+Stack - LIFO (з англ. Last-In-First-Out, “останій прийшов – перший пішов”)
+Queu - FIFO (з англ. First-In-First-Out, “перший прийшов – перший пішов”)
+Масиви у JS -  двобічна черга(елементи можна додавати і вадаляти як на початку, так і вкінці).
 */
 
-// string.slice(start, end) - end не включно
+// pop() - Видаляє останній елемент масиву та повертає його
 
-let stringToSlice = 'stringify';
-console.log(stringToSlice.slice(1, 4)); //tri
+console.log(fruits.pop()); // 'Plum'
+console.log(fruits); // ['Apple', 'Orange']
 
-// string.substring(start, end) - end не включно, майже те саме, але дозволяє start бути більшим за end, в такому разі міняє місцями
+// push(elem) - Додає елементи в кінець масиву, повертає його нову довжину
+console.log(fruits.push('Cherry')); // 3
+console.log(fruits); //  ['Apple', 'Orange', 'Cherry']
 
-console.log(stringToSlice.substring(2, 6)); // ring
-console.log(stringToSlice.substring(6, 2)); // ring
+// shift() - Видаляє перший елемент з масиву та повертає його
+console.log(fruits.shift()); // 'Apple'
+console.log(fruits); // ['Orange', 'Cherry']
 
-// string.substr(start, length) - повертає підрядок починаючи зі старт і заданої довжини
-// лише рушії браузерного Javascript мають його підтримувати, і не рекомендується його використовувати
-console.log(stringToSlice.substr(3, 3)); //ing
+// unshift() - Додає елементи в початок масиву, повертає його нову довжину
+console.log(fruits.unshift('Peach')); // 3
+console.log(fruits); //['Peach', 'Orange', 'Cherry']
 
-// string.codePointAt(pos) - повертає позицію у Юнікоді
-console.log('Z'.codePointAt(0)); // 90
-console.log('z'.codePointAt(0)); // 122
+// for .. of array loop // в 10 - 100 разів швидше за for .. in
 
-// string.fromCodePoint(code) - навпаки з номеру у Юнікоді відтворює символ
-console.log(String.fromCodePoint(90)); //Z
-console.log(String.fromCodePoint(122)); //z
-
-//Task вивести симполи з 1 по 250 одним рядком
-
-let allSymbols = '';
-for (let i = 1; i < 251; i++) {
-    allSymbols += String.fromCodePoint(i);
+for (let fruit of fruits) {
+    console.log(fruit);
 }
 
-console.log(allSymbols);
+// Отож, найпростіший метод очистки масиву це: arr.length = 0;.
 
-// Метод порівння рядків
-// string.localCompare(string2)
-// Повертає відʼємне число, якщо str менше, ніж str2.
-// Повертає додатне число, якщо str більше, ніж str2.
-// Повертає 0, якщо вони рівні.
-let stringMe = 'Suslyk';
-let stringHim = 'Tretyak';
+//new Array(number) - створює порожній масив відповідної довжени
 
-console.log(stringMe.localeCompare(stringHim)); // -1 => stringMe < stringHim
+// toString(array) - повертає рядокз елементів, які розділені комою
+console.log(fruits.toString()); //Peach,Orange,Cherry
 
-//Task 1
-// Напишіть функцію ucFirst(str), яка повертає рядок str з першим символом у верхньому регістрі
+//TAsks
+// 1 
+// Створіть масив styles з елементами “Jazz” та “Blues”.
+// Додайте “Rock-n-Roll” в кінець масиву.
+// Замініть значення в середині масиву на “Classics”. Ваш код для пошуку медіанного елемента має працювати для будь-яких масивів непарної довжини.
+// Видаліть перший елемент масиву та покажіть його.
+// Вставте Rap та Reggae на початок масиву.
 
-function ucFirst(str) {
-    if (!str) return str;
+//2
+const styles = ['Jazz', 'Blues'];
+styles.push('Rock-n-Roll');
+styles[Math.floor((styles.length - 1) / 2)] = 'Classics';
+console.log(styles.shift());
+styles.unshift('Rap', 'Raggie');
 
-    return str[0].toUpperCase() + str.slice(1);
+//3
+/* Напишіть функцію sumInput() яка:
+
+Просить користувача ввести дані за допомогою prompt та зберігає їх в масив.
+Закінчує робити запити в користувача після того, як введено не числове значення, порожня строка або натиснуто “відмінити”.
+Підраховує та повертає суму елементів масиву.
+P.S. Нуль 0 це – валідне число, будь ласка, не зупиняйте функцію при введені 0. */
+
+function sumInput() {
+    const inputsArray = [];
+    while (true) {
+        let elem = Number(prompt('Insert a number'));
+        if (!isFinite(elem) || elem === '' || elem === null) break;
+        inputsArray.push(elem);
+    }
+
+    let sum = 0;
+    for (element of array) {
+        sum += element;
+    }
+
+    return sum;
 }
 
-//Task 2
-//Напишіть функцію checkSpam(str), яка повертає true, якщо str містить ‘viagra’ or ‘XXX’, інакше false.
+//4
+/* На вході масив чисел, наприклад arr = [1, -2, 3, 4, -9, 6].
 
-function checkSpam(str) {
-    let lowerStr = str.toLowerCase();
-    return lowerStr.includes('viagra') || lowerStr.includes('xxx');
-}
+Завдання: знайти неперервний підмасив arr з максимальною сумою елементів.
 
-checkSpam('buy ViAgRA now'); // true
-checkSpam('free xxxxx'); // true
-checkSpam("innocent rabbit");  // false
+Написати функцію getMaxSubSum(arr) яка повертає таку суму.
 
-// Task 3
-//Створіть функцію truncate(str, maxlength), яка перевіряє довжину str і, якщо вона перевищує maxlength – замінює кінець str символом трьох крапок "…", щоб його довжина була рівною maxlength.
+Наприклад: 
 
-function truncate(str, maxlength) {
-    return (str.length > maxlength) ? str.slice(0, maxlength - 1) + '…' : str;
-}
+getMaxSubSum([-1, 2, 3, -9]) == 5 (the sum of highlighted items)
+getMaxSubSum([2, -1, 2, 3, -9]) == 6
+getMaxSubSum([-1, 2, 3, -9, 11]) == 11
+getMaxSubSum([-2, -1, 1, 2]) == 3
+getMaxSubSum([100, -9, 2, -3, 5]) == 100
+getMaxSubSum([1, 2, 3]) == 6 (take all)*/
 
-// Task 4
-// У нас є вартість у вигляді "$120". Тобто: спочатку йде знак долара, а потім число.
-// Створіть функцію extractCurrencyValue(str), яка витягне числове значення з такого рядка та поверне його.
+// - Approach 1 O(n^2) Time Solution(quadratic time)
 
-function extractCurrencyValue(string) {
-    return string.startsWith('$') ? Number(string.slice(1)) : Number(string);
+/* function getMaxSubSum(array){
+    let maxSum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        let maxSubarrayStart = 0;
+        for (let j = i; j < array.length; j++) {
+            maxSubarrayStart += array[j];
+            maxSum = Math.max(maxSubarrayStart, maxSum);
+        }
+    }
+
+    return maxSum;
+} */
+
+// Approuch 2 O(n) - linear time
+
+function getMaxSubSum(array) {
+    let maxSum = 0;
+    let partialSum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        partialSum = Math.max(partialSum + array[i], array[i]);
+        maxSum = Math.max(partialSum, maxSum);
+    }
+
+    return maxSum;
 }
