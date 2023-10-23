8c44f4f6d8865f5f87ad4d71b38108133d5c2b87
suslinhio <suslinhio@gmail.com>
2023-10-23 19:43:27 +0300

    Типи даних.Деструктуроване присвоєння.

--- index.js
+++ index.js
@@ -1,72 +1,154 @@
 'use strict' 
 
-// Object.keys, values, entries
+// Деструктуроване присвоєння
 
-/* 
-Методи keys(), values(), entries() підтримуються для:
+let arr = ['Dima','Suslyk'];
 
-- Map
-- Set
-- Arrays
+let [firstname, surname] = arr; //Деструктуроване присвоєння: встановлює array[0] v firstname, array[1] d surname
 
-Звичайні об’єкти також підтримують подібні методи, але синтаксис дещо інший.
+console.log(firstname);
+console.log(surname);
 
-Object.keys(obj) - повертає масив ключів.
-Object.values(obj) - повертає масив значень.
-Object.entries(obj) - повертає масив пар.
-*/
+let string = 'Ihor Tretyak';
+let [igorFirstname, igorSurname] = string.split(' ');
 
-/* У об’єктів немає багатьох методів, які є у масивів, наприклад map, filter та інші.
+console.log(igorFirstname);
+console.log(igorSurname);
 
-Якщо б ми хотіли їх застосувати, тоді б ми використовували Object.entries з подальшим викликом Object.fromEntries:
+let user = ['Artem', 'UnnecessaryInfo', 'Pushkar', 'other', 'otherAgain'];
+let [artemFirstname, , artemSurname] = user; //ignore unnecessary
 
-Викликаємо Object.entries(obj), щоб отримати масив пар ключ/значення з obj.
-На ньому використовуємо методи масиву, наприклад map, щоб перетворити ці пари ключів/значень.
-Використаємо Object.fromEntries(array) на отриманому масиві, щоб перетворити його знову на об’єкт. */
+console.log(artemFirstname);
+console.log(artemSurname);
 
-// task double the prices
+/* Насправді, ми можемо використовувати його з будь-якими даними, які перебираються, а не тільки з масивами: 
+- стрінгами
+- сетами та мепами
+ */
 
-const prices = {
-    banana: 1,
-    orange: 2,
-    meat: 4,
+//Обмін змінними за допомогою деструктурованого присвоєння - як у бульбашковому алгоритмі
+
+let guest = 'Yasya';
+let admin = 'Suslinhio';
+
+[guest, admin] = [admin, guest];
+
+console.log(guest);
+console.log(admin);
+
+//Деструктурування об’єктів
+
+const options = {
+    title: "Меню",
+    width: 100,
+    height: 200
 };
 
-function doublePrices(obj) {
-    return Object.fromEntries(Object.entries(obj).map(entry => [entry[0], entry[1] * 2]));
+let {title, width, height} = options;
+console.log(title);
+console.log(width);
+console.log(height);
+
+/* Якщо ми хочемо присвоїти властивість змінній з іншим іменем, наприклад, зробити так, щоб options.width переходив до змінної з назвою w, то ми можемо встановити ім’я змінної за допомогою двокрапки: */
+
+let {title: myNameForTitle, width: shyryna, height: vysota, area: a = 'unknown'} = options; // стандартне значення, для властивостей яких може не бути також робиться
+console.log(myNameForTitle);
+console.log(shyryna);
+console.log(vysota);
+console.log(a);
+
+//Якщо у нас є складний об’єкт з багатьма властивостями, ми можемо витягти лише те, що нам потрібно, вказавши лише відповідну назву властивості
+
+//Оператор ...rest  теж парцює
+
+let {title: titleAgain, ...values} = options;
+console.log(titleAgain);
+console.log(values); //{width: 100, height: 200}
+
+//Можна копіювати у вже існуючі змінні, але перед цим огорнути дужками()
+let men, w, h;
+
+({title: men, width: w, height: h} = options);
+console.log(men);
+console.log(h);
+console.log(w);
+
+//Вкладене деструктурування - якщо обєкт або масив містять інший обєкт або масив
+
+let options2D = {
+    size: {
+      width: 100,
+      height: 200
+    },
+    items: ["Торт", "Пончик"],
+    extra: true
+  };
+
+let {size: {width: width2, height: height2}, items: [item1, item2], extra: boolean, myAdditional = 100} = options2D;
+console.log(width2);
+console.log(height2);
+console.log(item1);
+console.log(item2);
+console.log(boolean);
+console.log(myAdditional);
+
+// Розумні параметри функції
+//Ми можемо передати параметри як об’єкт, і функція негайно деструктурує їх на змінні:
+let optionsMenu = {
+    title: "My menu",
+    items: ["Item1", "Item2"]
+  };
+
+function showMenu({title, width = 200, height = 350, items = []}) {
+    console.log(title, width, height);
+    console.log(items);
 }
 
-//Task 2
-/* Є об’єкт salaries з довільною кількістю властивостей, що містять заробітні плати.
+showMenu(optionsMenu);
 
-Напишіть функцію sumSalaries(salaries), що повертає суму всіх зарплат за допомогою Object.values та циклуfor..of.
+//Tasks
 
-Якщо об’єкт salaries порожній, тоді результат повинен бути 0. */
+//1
+/* Напишіть деструктуроване присвоєння, яке зчитує:
 
-const salaries = {
-    "Іван": 100,
-    "Петро": 300,
-    "Марія": 250
+властивість name у змінну name.
+властивість years у змінну age.
+властивість isAdmin у змінну isAdmin (false, якщо така властивість відсутня) */
+
+let user1 = {
+    name: "Іван",
+    years: 30
+  };
+
+let {name: name} = user1;
+console.log(name);
+let {years: age} = user1;
+console.log(age);
+let {isAdmin: isAdmin = false} = user1;
+console.log(isAdmin);
+
+//2
+/* Створіть функцію topSalary(salaries) яка повертає ім’я найбільш високооплачуваної особи.
+
+Якщо об’єкт salaries пустий, функція повинна повернути null.
+Якщо є кілька високооплачуваних осіб, поверніть будь-якого з них.
+P.S. Використовуйте Object.entries і деструктурування для перебору пар ключ/значення. */
+
+let salaries = {
+  "Іван": 100,
+  "Петро": 300,
+  "Марія": 250
 };
 
-function sumSalaries(obj) {
-    /* let sum = 0;
-    for (let key of Object.values(obj)) {
-        sum += key;
+function topSalary(obj) {
+  let maxSalary = 0;
+  let maxSalaryName = null;
+    for (let [name, salary] of Object.entries(obj)) {
+      if (salary > maxSalary) {
+        maxSalary = salary;
+        maxSalaryName = name;
+      }
     }
-
-    return sum; */
-    return Object.values(obj).reduce((accum, salarie) => accum + salarie, 0);
-}
-
-//Task 3
-/* Напишіть функцію count(obj), що повертає кількість властивостей об’єкта: */
-
-let user = {
-    name: 'Іван',
-    age: 30
-};
-
-function count(obj) {
-    return Object.keys(obj).length;
-}
+    return maxSalaryName;
+} 
+console.log(topSalary(salaries));
