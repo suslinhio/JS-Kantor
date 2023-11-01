6405d1789cf44fe6b517999b7a75a97f6cc93f77
suslinhio <suslinhio@gmail.com>
2023-11-01 16:32:12 +0200

    Розширена робота з функціями. Обєкт функції, NFE

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
 'use strict' 
 
-// Global object
+// Об’єкт функції, NFE
 
-/* Глобальний об’єкт містить змінні, які мають бути доступними скрізь.
+//Name
 
-Включаючи вбудовані у JavaScript, такі як Array, та значення, характерні для середовища, такі як window.innerHeight – висота вікна у браузері.
+function sayHi() {
+    console.log('Hi!');
+}
+console.log(sayHi.name); //sayHi
 
-Глобальний об’єкт має універсальну назву globalThis.
+//Length
 
-… Але частіше згадуються специфічні назви середовища “старої школи”, такі як window (браузер) та global (Node.js).
+function f1(a) {}
+function f2(a, b) {}
+function many(a, b, ...more) {}
 
-Ми повинні зберігати значення у глобальному об’єкті, лише якщо вони дійсно глобальні для нашого проєкту. І тримати їх кількість мінімальною.
+console.log(f1.length); // 1
+console.log(f2.length); // 2
+console.log(many.length); // 
 
-В браузерах, якщо ми не використовуємо модулі, глобальні функції та змінні, оголошені за допомогою var, стають властивістю глобального об’єкта.
 
-Для того, щоб код був простішим і в майбутньому його легше було підтримувати, слід звертатися до властивостей глобального об’єкта безпосередньо, через window.x. */
+//Tasks
+
+//1
+/* Змініть код makeCounter() так, щоб лічильник міг також зменшити та встановити рахунок:
+
+counter() повинен повернути наступний рахунок (як раніше).
+counter.set(value) повинен встановити лічильник в значення value.
+counter.decrease() повинен зменшити лічильник на 1. */
+
+function makeCounter() {
+    let count = 0;
+
+    
+
+    return function counter() {
+        counter.set = function (value) {
+            count = value;
+            return count;
+        }
+    
+        counter.decrease = function () {
+            return --count;
+        }
+
+        return ++count;
+    }
+}
+
+let counting = makeCounter();
+
+//2
+/* Напишіть функцію sum, яка б працювала так:
+
+sum(1)(2) == 3; // 1 + 2
+sum(1)(2)(3) == 6; // 1 + 2 + 3
+sum(5)(-1)(2) == 6
+sum(6)(-1)(-2)(-3) == 0
+sum(0)(1)(2)(3)(4)(5) == 15 */
+
+function sum(a) {
+
+    let currentSum = a;
+  
+    function f(b) {
+      currentSum += b;
+      return f;
+    }
+  
+    f.toString = function() {
+      return currentSum;
+    };
+  
+    return f;
+  }
